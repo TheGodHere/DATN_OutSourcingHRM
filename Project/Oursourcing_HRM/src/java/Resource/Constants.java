379fe0a7fde005919dd3e174f5422b5028cb054d
TheGodHere <hoanglmse60937@gmail.com>
2015-05-13 16:54:20 +0700

    update project, tạo khung sườn project

---
+++ Project/Oursourcing_HRM/src/java/Resource/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Resource;
+
+/**
+ *
+ * @author Le Minh Hoang
+ */
+public class Constants {
+    public final static String sqldir = ""; 
+    
+}
